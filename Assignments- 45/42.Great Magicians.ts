f4904964febfe2175807e2d1322f33e336e13fc8
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:52:55 -0800

    Add files via upload

---
+++ Assignments- 45/42.Great Magicians.ts
@@ -0,0 +1,25 @@
+// Task 42 
+// Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. 
+// Call show_magicians() to see that the list has actually been modified.
+
+//41
+
+function show_magicians(magician: string[]): void {
+    
+    for (const magicians of magician) {
+     console.log(magicians);
+    }
+ }
+//     const magicians: string [] = ['Harry Porter', 'Ron Weasley','Ali', 'Jawad','Aatif']; 
+//     show_magicians(magicians);
+   
+// 42
+   function make_great(magicians:string[]): void{
+for(let i=0; i< magicians.length; i++){
+magicians[i]= magicians[i] + ' the great magician.'
+}
+   }
+
+   const magicians2: string[]= ['Harry Porter', 'Rony','Ali','Doctor','Qurishi'];
+   make_great(magicians2)
+   show_magicians(magicians2)
