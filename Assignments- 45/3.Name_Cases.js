1b9362de5f84cbe5581881fc0fe6502a8ec1ea3c
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:21:29 -0800

    Add files via upload

---
+++ Assignments- 45/3.Name_Cases.js
@@ -0,0 +1,19 @@
+"use strict";
+// Task 3
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and 
+// titlecase.
+let personName = '';
+personName = prompt('what is your name?') || ('');
+let lowercase = personName.toLowerCase();
+let upercase = personName.toUpperCase();
+let titlecase = personName.split('').map(word => word.charAt(0).toUpperCase()
+    + word.slice(1).toLowerCase()).join('');
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, Here is your in:
+  lowercase: ${lowercase}
+  upercase:  ${upercase}
+  titlecase: ${titlecase}`);
+}
+else {
+    alert('Please fill your name!');
+}
---
+++ Assignments- 45/3.Name_Cases.ts
@@ -0,0 +1,24 @@
+// Task 3
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and 
+// titlecase.
+
+
+let personName : string ='';
+
+personName =prompt('what is your name?') || ('');
+
+let lowercase : string =  personName.toLowerCase();
+let upercase  : string =  personName.toUpperCase();
+
+let titlecase : string =  personName.split('').map( word=> word.charAt(0).toUpperCase() 
++ word.slice(1).toLowerCase()).join('')
+
+if(personName !== null && personName !== '') {
+      alert(`Hello ${personName}, Here is your in:
+  lowercase: ${lowercase}
+  upercase:  ${upercase}
+  titlecase: ${titlecase}`)
+}
+else{
+    alert('Please fill your name!')
+}
