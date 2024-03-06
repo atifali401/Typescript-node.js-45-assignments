29a547cd2f605ec8c152684f59a0331a63c04a7f
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:27:46 -0800

    Add files via upload

---
+++ Assignments- 45/10.Adding_Comments1.ts
@@ -0,0 +1,14 @@
+//Task 10
+
+// Coding program by Atif Ali
+// Current date : 14-02-2024
+
+// This program is just Math 
+ 
+console.log(4+3);
+
+console.log(10-3);
+
+console.log(14/2);
+
+console.log(3.5*2);
