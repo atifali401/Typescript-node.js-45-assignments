6e4d6459344f7839ac4b97eae9a026121c563c94
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:36:27 -0800

    Add files via upload

---
+++ Assignments- 45/21.Store in a TypeScript Object.js
@@ -0,0 +1,5 @@
+"use strict";
+// tsk 21
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let person = { name: 'Ali', fname: 'male', age: 36 };
+console.log(person);
