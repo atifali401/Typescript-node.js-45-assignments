24ba6fa1abebf12ec7bd332e1d1bc288edae9770
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:25:14 -0800

    Add files via upload

---
+++ Assignments- 45/9.Favorite Number.js
@@ -0,0 +1,7 @@
+"use strict";
+//Task 9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals 
+// your favorite number. Print that message.
+const favorite_number = 7;
+const message = 'Here is your favorite number ' + favorite_number;
+console.log(message);
---
+++ Assignments- 45/9.Favorite Number.ts
@@ -0,0 +1,11 @@
+//Task 9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals 
+// your favorite number. Print that message.
+
+
+
+const favorite_number : number = 7;
+
+const message : string = 'Here is your favorite number ' + favorite_number ;
+
+console.log( message);
