6ff1329b0ac39e40c8cb54b7e8c40bcb5d56f0cd
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:40:42 -0800

    Add files via upload

---
+++ Assignments- 45/25.Alien Colors #1.ts
@@ -0,0 +1,22 @@
+// Task 25
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign 
+//ita value of 'green', 'yellow', or 'red'.
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message
+// that the player just earned 5 points.
+
+//• Write one version of this program that passes the if test and another that fails. 
+//(The version that fails will have no output.)
+
+let alienColor: string ='green';
+  if(alienColor=== 'green'){
+      console.log('Player just earn 5 points!')
+  }
+    // alienColor = 'red'
+    // if(alienColor=== 'green'){
+    // console.log('Player just earn 5 points!');
+    // }
+    else
+     console.log('I am learning Coding.');
+
+
+     
