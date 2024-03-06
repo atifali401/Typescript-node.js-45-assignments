604e7be4239eb9002b95c27e464af9bfa4533f54
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:24:47 -0800

    Add files via upload

---
+++ Assignments- 45/8.Four_lines.ts
@@ -0,0 +1,11 @@
+//Task 8
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(2+6);  // addition 
+
+console.log(10-2); // subtractting
+
+console.log(4*2); // here we are multipuling
+
+console.log(16/2); // here we dividing
