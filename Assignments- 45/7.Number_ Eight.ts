537a57d1cbcf8e5cec7a7166c07dddc405ebacfd
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:24:08 -0800

    Add files via upload

---
+++ Assignments- 45/7.Number_ Eight.ts
@@ -0,0 +1,12 @@
+//Task 7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Be sure to enclose your operations in print statements to see the results.
+
+
+console.log(2+6);  // addition 
+
+console.log(10-2); // subtractting
+
+console.log(4*2); // here we are multipuling
+
+console.log(16/2); // here we dividing
