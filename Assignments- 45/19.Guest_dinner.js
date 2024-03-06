49ae2c7d01d001a7569a7225da122a5ccdd275cc
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:36:21 -0800

    Add files via upload

---
+++ Assignments- 45/19.Guest_dinner.js
@@ -0,0 +1,17 @@
+"use strict";
+let guestarr = ["Aftab", "joyo", "Hashim", "Jawad"];
+for (let i = 0; i < guestarr.length; i++) {
+    console.log(`Dear Mr,` + guestarr[i] + ` ,\n\n
+      I invited you for the dinner\n\nThankyou.\n\n`);
+}
+console.log(`Good news, I found out a bigger table so, i invited 3 more friends.The more the merrier, Wow now there will be my 7 friend, yahoooo!`);
+/* guestarr.unshift(`Habib`);
+ guestarr.splice(2,0,`Aatif`);
+ guestarr.push(`Asad`);
+
+ for(let i=0; i<guestarr.length; i++)
+{
+ console.log(`Dear Mr,`+ guestarr[i]+ `,\n\n
+ I invited you for the dinner.\n\nThankyou.\n\n`);
+
+}*/ 
---
+++ Assignments- 45/20.Think_Of.ts
@@ -0,0 +1,9 @@
+//Task 20 
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, 
+// cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let Language : string []= ['Urdu', 'English', 'Arbi', 'Parsi', 'German'];
+
+console.log("List of laguages",Language);
+
+//console.log(Language);
