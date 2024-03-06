4493530bacdf9891c6289461b3ad03deabc91f34
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 14:39:31 -0800

    Add files via upload

---
+++ Assignments- 45/23.Conditional Tests.js
@@ -0,0 +1,29 @@
+"use strict";
+// task 23
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction
+// for the results of each test. Your code should look something like this:
+/*let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True otherwise i predict False .");
+console.log(car == 'subaru');
+console.log(car == 'Civic');
+let num = 23;
+console.log("Is num == 23 (in number) ? I predict True otherwise i predict False ");
+console.log(num === 23);
+console.log(num === "23");
+let name1 = 'Ali';
+console.log(name1 === 'Ali');
+console.log(name1 === 'Zobi');
+let food = 'Apple';
+console.log(food === 'Apple');
+console.log(food === 'Mangu');
+let Score = 3.45;
+console.log(Score === 3.45);
+console.log(Score === 3.47);
